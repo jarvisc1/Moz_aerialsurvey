51141859fb733796610b01646423751e8cb09216
jarvisc1 <Christopher.Jarvis@lshtm.ac.uk>
2019-04-04 18:32:16 +0100

    Added relatvie spatial varibale definined on distances to roads and flood extents.

--- scripts/Dm-Survey-cleandata.R
+++ scripts/Dm-Survey-cleandata.R
@@ -5,6 +5,8 @@
 survey <- read_sf('data/raw/survey.geojson') %>% st_utm()
 rivers <- read_sf('data/raw/rivers.geojson') %>% st_utm()
 roads <- read_sf('data/raw/roads.geojson') %>% st_utm()
+flood <- read_sf('data/raw/flood_20190320.geojson') %>% st_utm()
+grid <- read_sf('data/raw/grid.geojson') %>% st_utm()
 
 survey
 
@@ -36,17 +38,36 @@ survey$sev_n <- as.numeric(survey$sev_f)
 survey$x_utm <- st_coordinates(survey)[,1] 
 survey$y_utm <- st_coordinates(survey)[,2]
 
-# Calculate if within 2km of river or roads
+# Calculate if within 1km of river or roads
 
 within_1km_road <- st_is_within_distance(survey, roads,  dist = 1000)
 within_1km_river <- st_is_within_distance(survey, rivers,  dist = 1000)
 
-survey$within_1km_road <-  ifelse(apply(within_1km_road, 1, sum)>0, 1,0)
-survey$within_1km_river <-  ifelse(apply(within_1km_river, 1, sum)>0, 1,0)
+survey$within_1km_road <-  ifelse(apply(within_1km_road, 1, sum)>0, "Within 1km road", "Further than 1km of road")
+survey$within_1km_river <-  ifelse(apply(within_1km_river, 1, sum)>0, "Within 1km river", "Further than 1km of river")
 survey$within_1km_river_road <-  survey$within_1km_river + survey$within_1km_road
-survey$within_1km_river_road_bin <-  (survey$within_1km_river_road >0)
+survey$within_1km_river_road_bin <-  ifelse((apply(within_1km_road, 1, sum)+apply(within_1km_river, 1, sum))>0,
+                                            "Within 1km road or river", "Further than 1km of road or river")
+table(survey$within_1km_river_road_bin)
+
+# Join the grids onto the survey points
+survey_grid <- st_join(survey, grid, join = st_within)
+
+# Pick grids surveyed within the flood extent
+floodindex <- c("F5", "F6", "E5", "E6", "D5", "D6", "C5", "C6", "B5", "B6", "A5", "A6")
+
+survey$flood <- ifelse(survey_grid$index %in% floodindex, "Flood", "Not visibly flooded")
+survey$worldpop <- survey_grid$worldpop
+survey$worldpopcats <- cut(survey$worldpop, breaks = c(0, 10000, 50000, 75000, 1e6), 
+                           labels = c("<10,000", "10,001 to 50,000", "50,001 to 75,000", "75,000+"))
+head(survey)
+head(survey_grid)
+# Put population data onto survey points
+
+
 
 saveRDS(survey, "data/survey.Rds")
+
 
 
 
--- scripts/Setup.R
+++ scripts/Setup.R
@@ -12,3 +12,4 @@ library(mapview)
 ## Define reprojection function for Zimbabawe UTM
 st_utm <- function(d) st_transform(d, crs = 32736)
 st_wgs84 <- function(d) st_transform(d, crs = 4326)
+
