ff54730920ced6ef2e4f4f1e793b9bd2760cd817
jarvisc1 <Christopher.Jarvis@lshtm.ac.uk>
2019-04-04 08:16:20 +0100

    Add setup script with packages and helpful functions

---
+++ scripts/Setup.R
@@ -0,0 +1,12 @@
+# Setup
+
+# Packages used in analysis
+
+library(sf)
+library(dplyr)
+library(forcats)
+
+# User defined functions
+## Define repojection function for Zimbabawe UTM
+st_utm <- function(d) st_transform(d, crs = 32736)
+st_wgs84 <- function(d) st_transform(d, crs = 4326)
